5544784ec20961a04f1a0413fc474317902338f1
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2024-08-03 05:55:23 -0400

    a

--- 02-iniciando-do-zero-com-php/02-03-comandos-de-saida/index.php
+++ 02-iniciando-do-zero-com-php/02-03-comandos-de-saida/index.php
@@ -7,18 +7,28 @@ fullStackPHPClassName("02.03 - Comandos de saída");
  */
 fullStackPHPClassSession("echo", __LINE__);
 
+echo "<h4>Weslley Gostoso</h4>";
+$hoje = Date("Y-m-d H:i:s");
+$hoje_object = new DateTime($hoje);
+$hoje_formatado = $hoje_object->format("d/m/Y");
+echo $hoje_formatado;
 
 /**
  * [ print ] https://php.net/manual/pt_BR/function.print.php
  */
 fullStackPHPClassSession("print", __LINE__);
 
-
+print "<h3>Melhorando o meu PHP</h3>";
 /**
  * [ print_r ] https://php.net/manual/pt_BR/function.print-r.php
  */
 fullStackPHPClassSession("print_r", __LINE__);
 
+$pessoas = array("p1" => "Fulano", "p2" => "Cicrano", "p3" => "Deltrano");
+
+echo "<pre>";
+print_r($pessoas);
+echo "</pre>";
 
 /**
  * [ printf ] https://php.net/manual/pt_BR/function.printf.php
